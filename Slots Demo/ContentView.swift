4fa6a8ae125fd1ce27e4183879627e40c74468bb
Robert Jackson Jr <robertjacksonj2@gmail.com>
2020-02-09 17:20:33 -0500

    Initial Commit

---
+++ Slots Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slots Demo
+//
+//  Created by Robert Jackson Jr on 2/9/20.
+//  Copyright © 2020 Robert Jackson Jr. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
